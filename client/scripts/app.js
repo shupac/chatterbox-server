5004b5a1c7813dcfb82ebc3b2ba275a0b5fe85d6
Hack Reactor Students <github@hackreactor.com>
2013-10-14 15:48:29 -0700

    Installed components. Copied in client code. Set up request handler server. Modified client code to GET and parse data from server.

---
+++ client/scripts/app.js
@@ -0,0 +1,144 @@
+// YOUR CODE HERE:
+var useURL = 'http://127.0.0.1:8080/1/classes/messages';
+
+var chatUser = window.location.search.split('=')[1];
+var _msgResults;
+var lastMsgTime = 0;
+var filter = 'lobby';
+var friends = {};
+var scrollPosition;
+
+
+var sendChat = function() {
+  var msg = {
+    "username": chatUser,
+    "text": $('.msgInput').val(),
+    'roomname': filter
+  };
+  var stringified = JSON.stringify(msg);
+  $.ajax({
+    url : useURL,
+    type : 'POST',
+    data : stringified,
+    contentType : 'application/json',
+    success : function() {
+      scrollPosition = undefined;
+      retrieve();
+    }
+  });
+  $('.msgInput').val('');
+};
+
+var retrieve = function(room) {
+  $.ajax({
+    url : useURL,
+    type : 'GET',
+    data : {
+      order: "-createdAt",
+      limit: 200
+    },
+    success : function(data) {
+      console.log('success');
+      data = JSON.parse(data);
+      _msgResults = data.results;
+      displayByRoom(filter);
+      buildChatRooms();
+    }
+  });
+};
+
+var buildChatRooms = function() {
+  var users = {};
+  var chatRooms = {};
+  $('.rooms').text('');
+  $('.users').text('');
+  for (var i = 0; i < _msgResults.length; i++) {
+    var room = _msgResults[i].roomname;
+    var user = _msgResults[i].username;
+    if (!chatRooms[room]) {
+      chatRooms[room] = 1;
+      $('.rooms').append('<li><a href="#" class="roomLink">' + escapeString(room) + '</a></li>');
+    } else {
+      chatRooms[room]++;
+    }
+
+    if (!users[user]) {
+      users[user] = 1;
+      var friendDisplay = 'none';
+      if(friends[user]) {
+        friendDisplay = 'inline';
+      }
+
+      $('.users').append('<li><a href="#" class="userLink">' + escapeString(user) + '</a><span class="friend" style="display:'+friendDisplay+'">*</span></li>');
+    }
+  }
+  $('a.roomLink').on('click', function(){
+    filter = $(this).text();
+    scrollPosition = undefined;
+    displayByRoom( filter );
+  });
+  $('a.userLink').on('click', function(){
+    var thisUser = $(this).text();
+    friends[thisUser] = !friends[thisUser];
+    $(this).parent().find('> .friend').toggle();
+    retrieve();
+  });
+  return chatRooms;
+};
+
+var displayByRoom = function(target) {
+  target = target || 'lobby';
+  $('ul.chatMsgs').text('');
+  var filtered = _(_msgResults).each(function(msgData){
+    if (msgData.roomname === target) {
+      var node = $('<li>' + escapeString(msgData.username, msgData) + ': ' + escapeString(msgData.text, msgData) + '</li>');
+      if(friends[msgData.username]) {
+        node.addClass('bold');
+      }
+      $('.chatMsgs').prepend(node);
+    }
+  });
+  if (scrollPosition === undefined) {
+    scrollPosition = $('ul.chatMsgs')[0].scrollHeight;
+  }
+  $('ul.chatMsgs').scrollTop(scrollPosition);
+  $('ul.chatMsgs').scroll(function() {
+    scrollPosition = $('ul.chatMsgs')[0].scrollTop;
+    // console.log(scrollPosition);
+  });
+};
+
+var escapeString = function(string, data) {
+  if (string !== undefined && string !== null) {
+    var returnString = '';
+    var specChars = {
+     "&": "&amp",
+     "<": "&lt",
+     ">": "&gt",
+     '"': "&quot",
+     "'": "&#x27",
+     '/': "&#x2F",
+     "$": "bling"
+    };
+    for (var i = 0; i < string.length; i++) {
+      if (specChars[ string[i] ] === undefined) {
+        returnString += string[i];
+      } else {
+        returnString += specChars[ string[i] ];
+      }
+    }
+    return returnString;
+  }
+};
+
+$(document).ready(function() {
+  retrieve();
+  setInterval(retrieve, 10000);
+
+  $('.msgInput').on('keypress', function(event) {
+    if (event.which === 13 && $('.msgInput').val() !== '') {
+      sendChat();
+    }
+  });
+
+});
--- server/request-handler.js
+++ server/request-handler.js
@@ -5,5 +5,24 @@
  * node module documentation at http://nodejs.org/api/modules.html. */
 
 var handleRequest = function(request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  var statusCode = 200;
 
+  var defaultCorsHeaders = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 
+  };
+
+  var headers = defaultCorsHeaders;
+
+  headers['Content-Type'] = "text/plain";
+
+  response.writeHead(statusCode, headers);
+  var responseJSON = {"results":[{"username":"anonymous","text":"Hi, I'm Emma","roomname":"lobby","createdAt":"2013-10-14T22:21:34.714Z","updatedAt":"2013-10-14T22:21:34.714Z","objectId":"vE6uIJezqp"}]};
+
+  response.end(JSON.stringify(responseJSON));
 };
+
+exports.handleRequest = handleRequest;
