dbac8978217db1cd9e3196327e73d925d104b568
Hack Reactor Students <github@hackreactor.com>
2013-10-15 11:44:46 -0700

    prepare for Heroku deployment

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var requestHandler = require("./request-handler.js");
-var port = 8080;
-var ip = "127.0.0.1";
+var port = process.env.port || 8080;
+var ip = process.env.IP || "127.0.0.1";
 var server = http.createServer(requestHandler.handleRequest).listen(port, ip);
 console.log("Listening on http://" + ip + ":" + port);
