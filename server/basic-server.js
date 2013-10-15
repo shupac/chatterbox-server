80c542971ef58228f170386a06863bded4d08f95
Hack Reactor Students <github@hackreactor.com>
2013-10-15 12:06:12 -0700

    edit port and ip for heroku

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,5 +2,5 @@ var http = require("http");
 var requestHandler = require("./request-handler.js");
 var port = process.env.port || 8080;
 var ip = process.env.IP || "127.0.0.1";
-var server = http.createServer(requestHandler.handleRequest).listen(port, ip);
+var server = http.createServer(requestHandler.handleRequest).listen(5000, '0.0.0.0');
 console.log("Listening on http://" + ip + ":" + port);
