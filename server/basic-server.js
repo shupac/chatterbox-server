81a9500313fb6baf978282c911837e61246e10c2
Hack Reactor Students <github@hackreactor.com>
2013-10-15 16:31:55 -0700

    Refactoring basic server using routing switch

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,7 +1,16 @@
 var http = require("http");
 var requestHandler = require("./request-handler.js");
-var port = process.env.port || 8080;
-var ip = process.env.IP || "127.0.0.1";
-var server = http.createServer(requestHandler.handleRequest);
-// console.log("Listening on http://" + ip + ":" + port);
+// var port = process.env.port || 8080;
+// var ip = process.env.IP || "127.0.0.1";
+
+
+var server = http.createServer(function(req, res){
+  switch (route) {
+    default:
+    requestHandler.handleRequest(req, res);
+    break;
+  }
+});
+
+
 server.listen(process.env.PORT || 5000);
