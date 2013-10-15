79dd753e007a777f5a3f29c9f12b88c8f2abc401
Hack Reactor Students <github@hackreactor.com>
2013-10-15 16:38:42 -0700

    Settting route parameter

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,10 +1,13 @@
 var http = require("http");
+var url = require('url');
 var requestHandler = require("./request-handler.js");
 // var port = process.env.port || 8080;
 // var ip = process.env.IP || "127.0.0.1";
 
 
 var server = http.createServer(function(req, res){
+  console.log(req.url);
+  var route = url.parse(req.url).pathname;
   switch (route) {
     default:
     requestHandler.handleRequest(req, res);
