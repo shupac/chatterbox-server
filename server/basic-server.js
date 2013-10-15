484963d311f83ad742a0bc676c7a89c7dba2bc39
Hack Reactor Students <github@hackreactor.com>
2013-10-15 16:26:04 -0700

    Refactoring basic server

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,5 +2,6 @@ var http = require("http");
 var requestHandler = require("./request-handler.js");
 var port = process.env.port || 8080;
 var ip = process.env.IP || "127.0.0.1";
-var server = http.createServer(requestHandler.handleRequest).listen(process.env.PORT || 5000);
-console.log("Listening on http://" + ip + ":" + port);
+var server = http.createServer(requestHandler.handleRequest);
+// console.log("Listening on http://" + ip + ":" + port);
+server.listen(process.env.PORT || 5000);
