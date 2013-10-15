ee5e4a61240597ee5ccd6e971e2ecbf5af4f88d2
Hack Reactor Students <github@hackreactor.com>
2013-10-15 16:02:51 -0700

    Refactoring basic server to handle routing logic

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,31 @@
 var http = require("http");
+var url = require('url');
 var requestHandler = require("./request-handler.js");
-var port = process.env.port || 8080;
-var ip = process.env.IP || "127.0.0.1";
-var server = http.createServer(requestHandler.handleRequest).listen(process.env.PORT || 5000);
-console.log("Listening on http://" + ip + ":" + port);
+
+
+// var port = process.env.port || 8080;
+// var ip = process.env.IP || "127.0.0.1";
+
+var server = http.createServer( function(req, res) {
+  var route = url.parse(req.url).pathname;
+  var handler;
+
+  switch(route) {
+    case 'classes/messages':
+      hander = requestHandler.handleRequest;
+      break;
+    default:
+      hander = requestHandler.handleRequest;
+      break;
+  }
+
+  handler.call();
+  handler.listen(process.env.PORT || 5000);
+});
+
+
+
+
+
+  // requestHandler.handleRequest).listen(process.env.PORT || 5000);
+// console.log("Listening on http://" + ip + ":" + port);
