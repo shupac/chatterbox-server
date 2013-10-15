aac089c8f7b653403f2913608aca6d58c2ba981b
Hack Reactor Students <github@hackreactor.com>
2013-10-15 12:09:08 -0700

    change port for Heroku (pt. 2)

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,5 +2,5 @@ var http = require("http");
 var requestHandler = require("./request-handler.js");
 var port = process.env.port || 8080;
 var ip = process.env.IP || "127.0.0.1";
-var server = http.createServer(requestHandler.handleRequest).listen(5000, '0.0.0.0');
+var server = http.createServer(requestHandler.handleRequest).listen(process.env.port || 5000);
 console.log("Listening on http://" + ip + ":" + port);
